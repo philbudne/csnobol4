87558619da827003ebf1d0c0ff49b79bf34e04af
Phil Budne <phil@regressive.org>
1993-12-09 12:05:04 +0000

    Initial revision

---
+++ main.c
@@ -0,0 +1,13 @@
+/* $Id$ */
+
+/* snobol4 main program */
+
+int
+main(argc, argv)
+    int argc;
+    char *argv[];
+{
+    init_args( argc, argv );
+    BEGIN( 0 );
+    return( 0 );
+}
---
+++ parms.h
@@ -0,0 +1,16 @@
+/* $Id$ */
+
+/* header files for snobol4.c */
+
+#include "types.h"
+#include "flags.h"
+#include "h.h"
+#include "libret.h"
+#include "syntab.h"
+#include "macros.h"
+#include "units.h"
+
+/* machine generated */
+#include "equ.h"
+#include "stg.h"
+#include "syn.h"
