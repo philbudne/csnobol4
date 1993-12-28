3978cccb8bde9b3f731f9b27c6457597cdfce109
Phil Budne <phil@regressive.org>
1993-12-28 15:37:45 +0000

    flushed flags.h, tidied up, renamed params.h

--- parms.h
+++ parms.h
@@ -2,15 +2,14 @@
 
 /* header files for snobol4.c */
 
-#include "types.h"
-#include "flags.h"
-#include "h.h"
-#include "libret.h"
-#include "syntab.h"
-#include "macros.h"
-#include "units.h"
+# include "types.h"
+# include "h.h"
+# include "libret.h"
+# include "syntab.h"
+# include "macros.h"
+# include "units.h"
 
 /* machine generated */
-#include "equ.h"
-#include "data.h"
-#include "syn.h"
+# include "equ.h"
+# include "data.h"
+# include "syn.h"
