42bc2b67c6ec3c877ff46aa6dc34ec90b95ae037
Phil Budne <phil@regressive.org>
1994-01-03 15:58:53 +0000

    Initial revision

---
+++ lib/posix/mstime.c
@@ -0,0 +1,23 @@
+/* $Id$ */
+
+#include "types.h"
+#include <sys/types.h>
+#include <sys/times.h>			/* appeared post-v7?! */
+#include <unistd.h>			/* for _SC_CLK_TCK */
+/*
+ * times() is venerable (it was in v6)!!
+ * use POSIX sysconf() to get hz!
+ */
+
+int_t
+mstime() {
+    struct tms tms;
+    int hz;
+
+    hz = sysconf(_SC_CLK_TCK);
+    if (hz == -1)
+	return 0;			/* just use 60?? */
+
+    times(&tms);
+    return(tms.tms_utime*1000/hz);	/* just user time? */
+}
