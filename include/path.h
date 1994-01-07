da7af91679fe91fa48077fdc21987859dc1aa945
Phil Budne <phil@regressive.org>
1994-01-07 14:28:22 +0000

    Initial revision

---
+++ include/path.h
@@ -0,0 +1,13 @@
+/* $Id$ */
+
+#ifndef LD_PATH
+#define LD_PATH "/usr/bin/ld"
+#endif
+
+#ifndef SNOLIB_PATH
+#define SNOLIB_PATH "/usr/local/lib/snolib"
+#endif
+
+#ifndef SNOLIB_A
+#define SNOLIB_A "snolib.a"
+#endif
