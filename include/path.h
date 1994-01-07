630662e94813599cc1299ffade28ef6c35d7e077
Phil Budne <phil@regressive.org>
1994-01-07 14:31:33 +0000

    fix SNOLIB_DIR

--- include/path.h
+++ include/path.h
@@ -1,13 +1,13 @@
 /* $Id$ */
 
 #ifndef LD_PATH
-#define LD_PATH "/usr/bin/ld"
-#endif
+#define LD_PATH "/usr/bin/ld"		/* new (SunOS4) fs org */
+#endif /* LD_PATH not defined */
 
-#ifndef SNOLIB_PATH
-#define SNOLIB_PATH "/usr/local/lib/snolib"
-#endif
+#ifndef SNOLIB_DIR
+#define SNOLIB_DIR "/usr/local/lib/snolib"
+#endif /* SNOLIB_DIR not defined */
 
 #ifndef SNOLIB_A
 #define SNOLIB_A "snolib.a"
-#endif
+#endif /* SNOLIB_A not defined */
