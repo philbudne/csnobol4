aa724fa4d66973352f9e65ce7b366488d5c55b9d
Phil Budne <phil@regressive.org>
1994-05-09 23:55:23 +0000

    Initial revision

---
+++ lib/vms/mstime.c
@@ -0,0 +1,17 @@
+/* $Id$ */
+
+#include "snotypes.h"
+#include <time.h>
+
+/*
+ * times() is venerable (it was in v6)!!  (But it seems to have changed over
+ * the years).
+ */
+
+int_t
+mstime() {
+    struct tbuffer tbuff;
+
+    times(&tbuff);
+    return(tbuff.proc_user_time * 10);		/* time in 10-ms units	*/
+}
