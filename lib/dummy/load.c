a6bcdb783a77cc9d51ad9e8e218c9d118b7bc09c
Phil Budne <phil@regressive.org>
1994-06-02 19:32:54 +0000

    call pml_{load,link}

--- lib/dummy/load.c
+++ lib/dummy/load.c
@@ -1,22 +1,33 @@
 /* $Id$ */
 
-/* dummy functions for LOAD/LINK/UNLOAD */
+/*
+ * dummy functions for LOAD/LINK/UNLOAD
+ * now allows user to link in functions at build time!
+ */
 
 int
 load(addr, sp1, sp2)
     struct descr *addr;			/* OUT */
     struct spec *sp1, *sp2;		/* function, library */
 {
+#ifdef NO_PML
     UNDF();
     /* NOTREACHED */
+#else  /* NO_PML not defined */
+    return pml_load(addr, sp1, sp2);
+#endif /* NO_PML not defined */
 }
 
 int
 link(retval, args, nargs, addr)
     struct descr *retval, *args, *nargs, *addr;
 {
+#ifdef NO_PML
     INTR10();
     /* NOTREACHED */
+#else  /* NO_PML not defined */
+    return pml_link(retval, args, nargs, addr);
+#endif /* NO_PML not defined */
 }
 
 void
