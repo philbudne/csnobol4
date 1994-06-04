2a735d691b9565e92d0f7b3f61b4ecb845861986
Phil Budne <phil@regressive.org>
1994-06-04 15:57:56 +0000

    more examples

--- pml.h
+++ pml.h
@@ -5,8 +5,10 @@
  * list of functions loaded with snobol4 image  
  */
 
-#if 0
-/* examples: */
+#if 0 /* examples: */
 PMLFUNC(SQRT)				/* see doc/sqrt.c */
+PMLFUNC(FILE)				/* see snolib/file.c */
+PMLFUNC(RENAME)				/* see snolib/rename.c */
+PMLFUNC(DELETE)				/* see snolib/delete.c */
 PMLFUNC2("FOO", bar)			/* entry point at "bar" */
 #endif /* 0 */
