95d3e6fd86f4b5ecc7f57c9d0472153926f13b01
Phil Budne <phil@regressive.org>
1994-06-23 16:58:53 +0000

    Pass "NORET" dummy descriptor instead off NULL. No need for NULL check when returning a value!! Add ENTER() and LEAVE() macros to support TRACE_DEPTH.

--- parms.h
+++ parms.h
@@ -1,15 +1,22 @@
 /* $Id$ */
 
-/* header files for snobol4.c; included by SIL "COPY PARAMS" macro */
+/* header files for snobol4.c; included by SIL "COPY PARMS" macro */
 
-# include "snotypes.h"
-# include "h.h"
-# include "libret.h"
-# include "syntab.h"
-# include "macros.h"
-# include "units.h"
+#include "snotypes.h"
+#include "h.h"
+#include "libret.h"
+#include "syntab.h"
+#include "macros.h"
+#include "units.h"
 
 /* machine generated */
-# include "equ.h"
-# include "data.h"
-# include "syn.h"
+#include "equ.h"
+#include "data.h"
+#include "syn.h"
+
+struct descr NORET[1];
+
+#ifdef TRACE_DEPTH
+int depth;
+int calls[1024];
+#endif /* TRACE_DEPTH defined */
