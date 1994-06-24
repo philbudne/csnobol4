1d437924bec3e4c19ed7e0c622d94eea2537f91a
Phil Budne <phil@regressive.org>
1994-06-24 15:03:59 +0000

    add MAX_DEPTH declare calls/depth here

--- lib/endex.c
+++ lib/endex.c
@@ -5,15 +5,31 @@
 #include "macros.h"
 #include "data.h"			/* for RETCOD */
 
+#ifdef TRACE_DEPTH
+#include <stdio.h>
+
+#define MAX_DEPTH 50000
+int depth;
+int calls[MAX_DEPTH];
+#endif /* TRACE_DEPTH defined */
+
 void
 endex( x )
     int x;
 {
+    int i;
+
     /* if &ABEND set, dump core?! */
     if (x) {
 	/* XXX perform I/O cleanup?! */
 	abort();
     }
+
+#ifdef TRACE_DEPTH
+    for (i = 0; i < MAX_DEPTH; i++)
+	if (calls[i])
+	    fprintf( stderr, "%8d %8d\n", i, calls[i]);
+#endif /* TRACE_DEPTH defined */
 
     /* else exit w/ &CODE */
     exit(D_A(RETCOD));
