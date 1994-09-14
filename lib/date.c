707cd80fd489908ec4ea7bb90299066d93704c27
Phil Budne <phil@regressive.org>
1994-09-14 16:53:53 +0000

    VMS tweak (from martin waller); ran new comdef

--- lib/date.c
+++ lib/date.c
@@ -1,11 +1,15 @@
 /* $Id$ */
 
-#include "h.h"
-#include "snotypes.h"
-#include "macros.h"
+# include "h.h"
+# include "snotypes.h"
+# include "macros.h"
 
-#include <sys/types.h>			/* time_t */
-#include <time.h>			/* struct tm */
+# ifdef VAXC
+# include <types.h>			/* time_t */
+# else  /* VAXC not defined */
+# include <sys/types.h>			/* time_t */
+# endif /* VAXC not defined */
+# include <time.h>			/* struct tm */
 
 /*
  * The format of DATE() is (in principle) system dependant,
