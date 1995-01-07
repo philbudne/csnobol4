6af8534bdae0c69649d1893b7891bef77c55303b
pbudne <>
1995-01-07 00:33:47 +0000

    add ANSI_STRINGS redefines

--- include/h.h
+++ include/h.h
@@ -19,3 +19,10 @@
 #define STRING(s) "s"
 #define CONC(a,b) a/**/b
 #endif /* __STDC__ not defined */
+
+#ifdef ANSI_STRINGS
+#define index(a,b) strchr(a,b)
+#define bcmp(a,b,c) memcmp(a,b,c)
+/* for bzero() use lib/aux/bzero.c (faster than memset) */
+/* for bcopy() use lib/aux/bcopy.c (fast, handles overlap correctly) */
+#endif /* ANSI_STRINGS */
