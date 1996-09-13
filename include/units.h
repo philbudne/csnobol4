66ab937d10a55c2bcbe6c2bb2462714f04bb3c08
Phil Budne <phil@regressive.org>
1996-09-13 03:41:52 +0000

    add UNITT for TERMINAL input

--- include/units.h
+++ include/units.h
@@ -3,4 +3,5 @@
 /* NOTE: must be .GE. 1 and .LE. 99!! */
 #define UNITI	5		/* INPUT: stdin */
 #define UNITO	6		/* OUTPUT: stdout */
-#define UNITP	7		/* PUNCH: stderr */
+#define UNITP	7		/* TERMINAL output: stderr */
+#define UNITT	8		/* TERMINAL input */
