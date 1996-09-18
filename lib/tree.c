11e88edf60ccc8ec0d6a6faa58390eb7342db0ac
Phil Budne <phil@regressive.org>
1996-09-18 03:36:10 +0000

    updated INSERT from SNOBOL4+ version

--- lib/tree.c
+++ lib/tree.c
@@ -45,17 +45,24 @@ void
 insert( d1, d2 )
     struct descr *d1, *d2;
 {
-    int_t a1, a2, a3, a4;
+    int_t a1, a2, a3, a4, x, y;
 
     a1 = D_A(d1);
     a2 = D_A(d2);
     a3 = D_A(a1 + FATHER);
     a4 = D_A(a3 + LSON);
 
-    D(a2 + FATHER) = D(a1 + FATHER);
-    D(a1 + FATHER) = D(d2);
-    D(a4 + RSIB) = D(d2);
+    /* updated from SNOBOL4+ version; */
+    x = a4;
+    do {
+	y = x;
+	x = D_A(y + RSIB);
+    } while (x != a1);
+    D(y + RSIB) = D(d2);
+
+    D(a2 + FATHER) = D(a3);
     D(a2 + LSON) = D(d1);
     D_V(a2 + CODE)++;
+    D(a1 + FATHER) = D(d2);
 }
 
