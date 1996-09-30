710f52dc2da6b2ce2108957cf00f297ca12d4a37
Phil Budne <phil@regressive.org>
1996-09-30 02:52:26 +0000

    renamed EXP2 due to globals conflict w/ snobol4.c

--- lib/snolib/exp.c
+++ lib/snolib/exp.c
@@ -2,7 +2,7 @@
 
 /*
  * SNOBOL4+ compatibility;
- * LOAD("EXP(REAL)REAL")
+ * LOAD("EXP2(REAL)REAL")
  * exponential e ** x
  */
 
@@ -13,7 +13,7 @@
 
 #include <math.h>
 
-EXP( LA_ALIST ) LA_DCL
+EXP2( LA_ALIST ) LA_DCL
 {
     RETREAL( exp( LA_REAL(0) ) );
 }
