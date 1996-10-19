42bc28f1e1caf6e26f37007d5335415069e32303
Phil Budne <phil@regressive.org>
1996-10-19 07:21:30 +0000

    Initial revision

---
+++ lib/posix/tty.c
@@ -0,0 +1,69 @@
+/* $Id$ */
+
+#include <stdio.h>
+#include <termios.h>
+#include <unistd.h>
+
+/*
+ * tty mode, echo
+ * POSIX.1 version
+ */
+
+static struct termios old;
+static int lastcbreak, lastnoecho, lastfd;
+
+void
+tty_save()
+{
+    tcgetattr(STDIN_FILENO, &old);	/* XXX stdin??? */
+    lastfd = -1;
+}
+
+void
+tty_restore()
+{
+    tcsetattr(STDIN_FILENO, TCSADRAIN, &old); /* XXX stdin??? */
+}
+
+void
+tty_mode( fp, cbreak, noecho )
+    FILE *fp;
+    int cbreak, noecho;
+{
+    struct termios new;
+    int fd;
+
+    fd = fileno(fp);
+    if (fd == lastfd && cbreak == lastcbreak && noecho == lastnoecho)
+	return;
+
+#if 1
+    /* what if not stdin (ie; some other tty)???
+     * BUT last "raw" request could have crushed members shared by VMIN/VTIME
+     */
+    new = old;
+#else
+    tcgetattr(fd, &new);
+#endif
+
+    if (cbreak) {
+	new.c_lflag &= ~ICANON;		/* kill canon; leave signals */
+#if 0
+	new.c_iflag &= ~(ICRNL|INLCR);	/* hey, system! leave CR/LF alone! */
+#endif
+	new.c_cc[VMIN] = 1;		/* want at least one */
+	new.c_cc[VTIME] = 0;		/* no minimum */
+    }
+    else {
+	/* XXX restore normality, what ever that was??? */
+    }
+
+    if (noecho)
+	new.c_lflag &= ~ECHO;		/* kill echo */
+
+    tcsetattr(fd, TCSADRAIN, &new);
+
+    lastcbreak = cbreak;
+    lastnoecho = noecho;
+    lastfd = fd;
+}
