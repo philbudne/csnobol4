897c6eba9177f7e91f9861b4612b51fe9a6c430f
Phil Budne <phil@regressive.org>
1996-10-20 07:09:45 +0000

    Initial revision

---
+++ lib/dummy/inet.c
@@ -0,0 +1,22 @@
+/* $Id$ */
+
+/* dummy inet interface */
+
+#include <stdio.h>
+
+FILE *
+tcp_open( host, service, port, priv )
+    char *host, *service;
+    int port, priv;
+{
+    return NULL;
+}
+
+
+FILE *
+udp_open( host, service, port, priv )
+    char *host, *service;
+    int port, priv;
+{
+    return NULL;
+}
