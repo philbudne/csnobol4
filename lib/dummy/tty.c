a1acc8ecd37496f783a78ae6a444036e0262d986
Phil Budne <phil@regressive.org>
1996-10-23 03:48:12 +0000

    Initial revision

---
+++ lib/dummy/tty.c
@@ -0,0 +1,40 @@
+/* $Id$ */
+
+#include <stdio.h>
+
+/*
+ * tty mode, echo
+ * dummy version
+ */
+
+int
+fisatty(f)
+    FILE *f;
+{
+    return FALSE;			/* why not? */
+}
+
+void
+tty_save()
+{
+}
+
+void
+tty_restore()
+{
+}
+
+void
+tty_mode( fp, cbreak, noecho, recl )
+    FILE *fp;
+    int cbreak, noecho, recl;
+{
+}
+
+/* advisory notice */
+void
+tty_close(f)
+    FILE *f;
+{
+    /* should not be called (fisatty returns FALSE) */
+}
