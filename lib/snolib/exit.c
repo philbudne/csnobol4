4a50ae24ce4cc2be122801f33c5b9e1b775c8cbb
Phil Budne <phil@regressive.org>
1996-10-24 03:32:29 +0000

    moved work to execute() function

--- lib/snolib/exit.c
+++ lib/snolib/exit.c
@@ -17,26 +17,14 @@
 #include "load.h"			/* LA_xxx macros */
 #include "equ.h"			/* datatypes I/S */
 
-#ifndef SH_PATH
-#define SH_PATH "/bin/sh"
-#endif
-
-#ifndef SH_PATH2
-#define SH_PATH2 "/usr/bin/sh"
-#endif
-
 int
 EXIT( LA_ALIST ) LA_DCL
 {
     char buf[512];			/* XXX */
-    char *env;
-    int n;
 
     if (LA_TYPE(0) == S) {		/* EXIT("command") */
 	getstring( LA_PTR(0), buf, sizeof(buf));
-	execl(SH_PATH, "sh", "-c", buf, 0); /* XXX SH_NAME? */
-	/* must not have found shell; try alternate */
-	execl(SH_PATH2, "sh", "-c", buf, 0); /* XXX SH_NAME2? */
+	execute(buf);			/* should not return */
 	/* ~sigh~ */
 	RETFAIL;
     }
