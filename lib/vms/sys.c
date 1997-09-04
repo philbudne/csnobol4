f88a299ea8fbde8c72adf02f6486b4bb2b9ec1be
Phil Budne <phil@regressive.org>
1997-09-04 02:36:58 +0000

    Initial revision

---
+++ lib/vms/sys.c
@@ -0,0 +1,23 @@
+/* $Id$ */
+
+/* support for HOST() on VMS */
+
+#ifdef vax
+#define HWNAME "vax"
+#else
+#define HWNAME "alpha"			/* lower case, like OSF/1 uname */
+#endif
+
+void
+hwname(cp)
+    char *cp;
+{
+    strcpy(cp, HWNAME);
+}
+
+void
+osname(cp)
+    char *cp;
+{
+    strcpy(cp, "VMS");			/* XXX get version */
+}
