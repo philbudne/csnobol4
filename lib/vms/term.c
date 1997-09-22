3ae419b1e55a4d1bd39513ec8c3e1ce877916772
Phil Budne <phil@regressive.org>
1997-09-22 22:05:06 +0000

    use SYS$COMMAND

--- lib/vms/term.c
+++ lib/vms/term.c
@@ -5,11 +5,9 @@
 /*
  * return a stdio stream for TERMINAL input variable
  * VMS version.
- *
- * XXX VAX C RTL has fdopen()!!!
  */
 
 FILE *
 term_input() {
-    return fopen("SYS$INPUT:", "r");	/* UNTESTED: this right? */
+    return fopen("SYS$COMMAND:", "r");	/* this right? */
 }
