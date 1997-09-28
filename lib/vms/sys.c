dd99c3f948bec9bf342ebcdd937adcb56422bee5
Phil Budne <phil@regressive.org>
1997-09-28 00:51:31 +0000

    *** empty log message ***

--- lib/vms/sys.c
+++ lib/vms/sys.c
@@ -3,11 +3,17 @@
 /* support for HOST() on VMS */
 
 #ifdef vax
-#define HWNAME "vax"
-#else
-/* safe, until other systems infected */
+#define HWNAME "VAX"
+#define OSNAME "VMS"			/* see osname() below */
+#else  /* vax not defined */
+#ifdef __ALPHA
 #define HWNAME "alpha"			/* lower case, like OSF/1 uname */
-#endif
+#define OSNAME "OpenVMS"
+#else  /* __ALPHA not defined */
+#define HWNAME "???"
+#define OSNAME "VMS"
+#endif /* __ALPHA not defined */
+#endif /* vax not defined */
 
 void
 hwname(cp)
@@ -20,5 +26,7 @@ void
 osname(cp)
     char *cp;
 {
-    strcpy(cp, "VMS");			/* XXX get version (using $GETSYIW?) */
+    /* XXX get version (using $GETSYIW? or DCL F$GETSYI("VERSION")?? */
+    /* C-Kermit does SYI$SYI_ARCH_NAME (4454) to test for "OpenVMS" on VAX */
+    strcpy(cp, OSNAME);
 }
