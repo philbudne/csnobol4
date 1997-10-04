c10144832eeaf1571fb77c9236142156ebd38202
Phil Budne <phil@regressive.org>
1997-10-04 03:16:43 +0000

    use GETSYIW to get architecture and version

--- lib/vms/sys.c
+++ lib/vms/sys.c
@@ -2,6 +2,12 @@
 
 /* support for HOST() on VMS */
 
+#include <syidef.h>
+
+#ifndef SYI$_ARCH_NAME
+#define SYI$_ARCH_NAME 4454
+#endif /* SYI$_ARCH_NAME not defined */
+
 #ifdef vax
 #define HWNAME "VAX"
 #define OSNAME "VMS"			/* see osname() below */
@@ -15,18 +21,80 @@
 #endif /* __ALPHA not defined */
 #endif /* vax not defined */
 
+struct item {
+    short buflen;			/* result buffer length */
+    short code;				/* item code */
+    char *buf;				/* result buffer */
+    long *len;				/* result length */
+    long zero;
+};
+
+#define MAXSTR 32
+static char _hwname[MAXSTR];
+static char _osname[MAXSTR+1+MAXSTR];
+
+static void
+sys_init() {
+    static int inited;
+    struct item item;
+    char temp[MAXSTR];
+    long len;
+    char *os, *hw;
+
+    if (inited)
+	return;
+
+    item.code = SYI$_ARCH_NAME;		/* "hw_arch" */
+    item.buflen = sizeof(temp) - 1;
+    item.buf = temp;
+    item.len = &len;
+    item.zero = 0;
+
+    os = OSNAME;
+    hw = HWNAME;
+    if ((SYS$GETSYIW(0, 0, 0, &item, 0, 0, 0) & 1) == 1) {
+	os = "OpenVMS";
+	temp[len] = '\0';
+	hw = temp;
+    }
+    else
+	os = "VMS";
+    strcpy(_hwname, hw);
+
+    item.code = SYI$_VERSION;
+    item.buflen = sizeof(temp) - 1;
+    item.buf = temp;
+    item.len = &len;
+    item.zero = 0;
+    if ((SYS$GETSYIW(0, 0, 0, &item, 0, 0, 0) & 1) == 1) {
+	temp[len] = '\0';
+	sprintf(_osname, "%s %s", os, temp);
+    }
+    else
+	strcpy(_osname, os);
+}
+
 void
 hwname(cp)
     char *cp;
 {
-    strcpy(cp, HWNAME);
+    sys_init();
+    strcpy(cp, _hwname);
 }
 
 void
 osname(cp)
     char *cp;
 {
-    /* XXX get version (using $GETSYIW? or DCL F$GETSYI("VERSION")?? */
-    /* C-Kermit does SYI$SYI_ARCH_NAME (4454) to test for "OpenVMS" on VAX */
-    strcpy(cp, OSNAME);
+    sys_init();
+    strcpy(cp, _osname);
 }
+
+#ifdef TEST
+main() {
+    sys_init();
+
+    printf("OS: %s\n", _osname);
+    printf("HW: %s\n", _hwname);
+}
+#endif
