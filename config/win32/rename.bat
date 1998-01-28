9321823ee265f8fb266c5258a302dc3b9cbffc87
Phil Budne <phil@regressive.org>
1998-01-28 18:44:48 +0000

    Initial revision

---
+++ config/win32/rename.bat
@@ -0,0 +1,8 @@
+rem $Id$
+
+rem batch file to fix truncated filenames
+
+rename data_ini.c data_init.c
+rename data_ini.h data_init.h
+rename lib\snolib\getstrin.c lib\snolib\getstring.c
+rename lib\snolib\retstrin.c lib\snolib\retstring.c
