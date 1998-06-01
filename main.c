12eac896b9d6ae425a0fc1933ec9d2714f5f3074
Phil Budne <phil@regressive.org>
1998-06-01 04:22:15 +0000

    allocate vars struct; call init_syntab

--- main.c
+++ main.c
@@ -2,12 +2,29 @@
 
 /* snobol4 main program (make this mlink.c??) */
 
+# ifdef NO_STATIC_VARS
+# include "h.h"
+# include "snotypes.h"
+# include "vars.h"
+struct vars *varp;
+# undef argc
+# undef argv
+# endif /* NO_STATIC_VARS defined */
+
 int
 main(argc, argv)
     int argc;
     char *argv[];
 {
+# ifdef NO_STATIC_VARS
+    varp = (struct vars *)malloc(sizeof(struct vars));
+    if (!varp) {
+	perror("could not allocate vars");
+	exit(1);
+    }
+# endif /* NO_STATIC_VARS defined */
     init_data();
+    init_syntab();
     init_args( argc, argv );
     version();
     BEGIN( 0 );
