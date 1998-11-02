a2e24d4c4f2728cca58e9e246d9bb4ad93e8fc5d
Phil Budne <phil@regressive.org>
1998-11-02 04:00:53 +0000

    avoid negative runtime deltas

--- lib/bsd/mstime.c
+++ lib/bsd/mstime.c
@@ -15,8 +15,23 @@
 int_t
 mstime() {
     struct rusage ru;
+#ifndef NO_GETRUSAGE_BUG
+    unsigned long x;
+    static unsigned long last = 0;	/* XXX belongs in vars.h? */
+#endif /* NO_GETRUSAGE_BUG not defined */
 
     getrusage( RUSAGE_SELF, &ru );	/* XXX check return? */
-    return(ru.ru_utime.tv_sec * 1000 +
-	   ru.ru_utime.tv_usec / 1000);
+    x = ru.ru_utime.tv_sec * 1000 + ru.ru_utime.tv_usec / 1000;
+
+#ifndef NO_GETRUSAGE_BUG
+    /*
+     * Ensure we'll never give negative deltas;
+     * BSD/OS 3.1 on 486's can, due to "statistical sampling"
+     * Older versions of NetBSD could as well.
+     */
+    if (x < last)
+	x = last;
+#endif /* NO_GETRUSAGE_BUG not defined */
+
+    return x;
 }
