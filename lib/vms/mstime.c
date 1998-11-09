b3c3597f7617038f2b2ca74150b6999cf11fd058
Phil Budne <phil@regressive.org>
1998-11-09 21:10:41 +0000

    use CLK_TCK if available

--- lib/vms/mstime.c
+++ lib/vms/mstime.c
@@ -11,12 +11,19 @@
 /*
  * times() is venerable (it was in v6)!!
  * (But it seems to have changed over the years).
+ *
+ * VMS "tbuffer" members: {proc,child}_{user,system}_time
+ * in CLK_TCK units
  */
+
+#ifndef CLK_TCK
+#define CLK_TCK 100
+#endif /* CLK_TCK not defined */
 
 int_t
 mstime() {
     struct tbuffer tbuff;
 
     times(&tbuff);
-    return(tbuff.proc_user_time * 10);		/* time in 10-ms units	*/
+    return(tbuff.proc_user_time * 1000 / CLK_TCK);
 }
