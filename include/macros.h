4e898184744e218d9cbb34841a436c0e05a5abd7
Phil Budne <phil@regressive.org>
2001-01-14 04:45:00 +0000

    add --longlong

--- include/macros.h
+++ include/macros.h
@@ -32,7 +32,7 @@
 #endif /* DCMP_BYTES not defined */
 
 /* clear B+1 descriptor block */
-#define ZERBLK(A,B) bzero(A, (B)+DESCR)
+#define ZERBLK(A,B) bzero((void *)(A), (B)+DESCR)
 
 /*
  * copy descriptor block
@@ -40,7 +40,7 @@
  * NOTE: may overlap!!
  * (bcopy deals with this but some memcpy's do not)!!!
  */
-#define MOVBLK(A,B,C) bcopy( (B)+DESCR, (A)+DESCR, (C) )
+#define MOVBLK(A,B,C) bcopy( (void *)((B)+DESCR), (void *)((A)+DESCR), (C) )
 
 /****************
  * string specifiers (qualifiers)
