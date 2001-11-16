ea5007d8b9eb1db947e0ec5963b0f84f4356d1c6
Phil Budne <phil@regressive.org>
2001-11-16 03:58:14 +0000

    Initial revision

---
+++ lib/ansi/spcint.c
@@ -0,0 +1,55 @@
+/* $Id$ */
+
+/*
+ * convert from strings to integer using strtol
+ * strtol are in SVID2, XPG2, XPG3, ANSI C
+ */
+
+#include "h.h"
+#include "snotypes.h"
+#include "macros.h"
+
+#include "equ.h"			/* for I and R */
+#include "res.h"
+#include "data.h"
+
+#ifdef NO_STATIC_VARS
+#include "vars.h"
+#endif /* NO_STATIC_VARS defined */
+
+int
+spcint(dp, sp)
+    struct descr *dp;
+    struct spec *sp;
+{
+    char buffer[64];			/* ??? */
+    int_t len;
+    char *cp;
+    int_t temp;
+
+    len = S_L(sp);
+    cp = S_SP(sp);
+
+    if (D_A(SPITCL)) {			/* SPITBOL features? */
+	/* strip leading whitespace */
+	while (len > 0 && (*cp == ' ' || *cp == '\t')) {
+	    cp++;
+	    len--;
+	}
+    }
+
+    if (len > sizeof(buffer)-1)
+	len = sizeof(buffer)-1;
+    bcopy( cp, buffer, len );
+    buffer[len] = '\0';
+
+    temp = strtol( buffer, &cp, 10);	/* always decimal */
+    if (*cp)
+	return FALSE;			/* failure */
+
+    D_A(dp) = temp;
+    D_F(dp) = 0;			/* clear flags */
+    D_V(dp) = I;			/* set type */
+
+    return TRUE;			/* success */
+}
---
+++ lib/ansi/spreal.c
@@ -0,0 +1,56 @@
+/* $Id$ */
+
+/*
+ * convert from string to real using strtod
+ * strtod are in SVID2, XPG2, XPG3, ANSI C
+ */
+
+#include "h.h"
+#include "snotypes.h"
+#include "macros.h"
+
+#include "equ.h"			/* for I and R */
+#include "res.h"
+#include "data.h"
+
+#ifdef NO_STATIC_VARS
+#include "vars.h"
+#endif /* NO_STATIC_VARS defined */
+
+int
+spreal(dp, sp)
+    struct descr *dp;
+    struct spec *sp;
+{
+    char buffer[64];			/* ??? */
+    int len;
+    char *cp;
+    float temp;
+    double strtod();
+
+    len = S_L(sp);
+    cp = S_SP(sp);
+
+    if (D_A(SPITCL)) {			/* SPITBOL features? */
+	/* strip leading whitespace */
+	while (len > 0 && (*cp == ' ' || *cp == '\t')) {
+	    cp++;
+	    len--;
+	}
+    }
+
+    if (len > sizeof(buffer)-1)
+	len = sizeof(buffer)-1;
+    bcopy( cp, buffer, len );
+    buffer[len] = '\0';
+
+    temp = strtod( buffer, &cp );
+    if (*cp)
+	return FALSE;			/* failure */
+
+    D_RV(dp) = temp;
+    D_F(dp) = 0;			/* clear flags */
+    D_V(dp) = R;			/* set type */
+
+    return TRUE;			/* success */
+}
