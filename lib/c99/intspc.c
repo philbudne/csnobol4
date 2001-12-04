9dc856414d457d9c8b5fbd691158b1d782c4ac3c
Phil Budne <phil@regressive.org>
2001-12-04 16:12:42 +0000

    Initial revision

---
+++ lib/c99/intspc.c
@@ -0,0 +1,35 @@
+/* $Id$ */
+
+/*
+ * convert from integer to string using printf %lld (in ISO C99)
+ */
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif /* HAVE_CONFIG_H defined */
+
+#include "h.h"
+#include "snotypes.h"
+#include "macros.h"
+
+#include "equ.h"
+
+#ifdef NO_STATIC_VARS
+#include "vars.h"
+#else
+static char strbuf[32];			/* 2^64 is only 21 chars! */
+#endif
+
+void
+intspc(sp, dp)
+    struct spec *sp;
+    struct descr *dp;
+{
+    sprintf( strbuf, "%lld", D_A(dp) );
+    S_A(sp) = (int_t) strbuf;		/* OY! */
+    S_F(sp) = 0;
+    S_V(sp) = 0;
+    S_O(sp) = 0;
+    S_L(sp) = strlen(strbuf);
+    CLR_S_UNUSED(sp);
+}
