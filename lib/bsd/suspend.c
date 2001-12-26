d843c5953bce651c823d8be41a39c7f8a0cb7ba2
Phil Budne <phil@regressive.org>
2001-12-26 01:51:33 +0000

    Initial revision

---
+++ lib/bsd/suspend.c
@@ -0,0 +1,28 @@
+/* $Id$ */
+
+/*
+ * process suspend
+ * BSD4.2 version
+ */
+
+#include <signal.h>
+
+void
+proc_suspend()
+{
+    sigset_t set;
+    
+    /* let default action occur */
+    signal(SIGTSTP, SIG_DFL);
+
+    /* Unblock SIGTSTP */
+    setsetmask(0);			/* new in 4.2 (test for sigmask?) */
+
+    /*
+     * Redeliver SIGTSTP to process group.  It's simpler to just
+     * deliver a SIGSTOP (no need for the signal mask monkeyshines,
+     * (or this system dependant function) but the message that the
+     * shell prints is different.
+     */
+    kill(0, SIGTSTP);
+}
