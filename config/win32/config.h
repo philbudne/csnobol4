670e4646526308831d9df16f27b3873d7aff17e6
Phil Budne <phil@regressive.org>
2002-01-27 05:01:59 +0000

    Initial revision

---
+++ config/win32/config.h
@@ -0,0 +1,21 @@
+/* $Id$ */
+
+/* Windows32 Visual C config.h */
+
+/*
+ * by default __STDC__ is undefined!?
+ *	-Za defines __STDC__ to 1, but that causes includes to turn
+ *	off useful non-ANSI extensions
+ */
+#define __STDC__	0
+
+#define ANSI_STRINGS
+#define NEED_POPEN_DECL
+#define SIGFUNC_T	void __cdecl
+#define SNOLIB_FILE	"snolib.dll"
+#define SNOLIB_DIR	"/snobol"
+#define USE_WINSOCK_H
+#define USE_STDARG_H
+
+#define finite		_finite
+#define isnan		_isnan
