e598fdf629483c391def988bcb1668b1d4407ea1
Phil Budne <phil@regressive.org>
2002-02-05 17:54:38 +0000

    fix comments use LA_STR_xxx macros fail on null string

--- lib/snolib/ord.c
+++ lib/snolib/ord.c
@@ -1,3 +1,13 @@
+/* $Id$ */
+
+/*
+ * LOAD("ORD(STRING)INTEGER")
+ *
+ * Usage;	ORD(STRING)
+ * Returns;	ordinal value of first character of STRING
+ *		CHAR(ORD('X')) should return 'X'
+ */
+
 #ifdef HAVE_CONFIG_H
 #include "config.h"
 #endif /* HAVE_CONFIG_H defined */
@@ -8,18 +18,9 @@
 #include "load.h"
 #include "equ.h"
 
-/*
- * LOAD("ORD(STRING)INTEGER")
- *
- * Usage;	ORD(STRING)
- * Returns;	ordinal value of first character of STRING
- *		CHAR(ORD('X')) should return 'X'
- */
-
 ORD( LA_ALIST ) LA_DCL
 {
-    char str[2];
-    getstring( LA_PTR(0), str, sizeof(str) );
-    /* sigh; this returns zero for the empty string.  FAIL instead?? */
-    RETINT( (unsigned char)str[0] );
+    if (LA_PTR(0) == NULL || LA_STR_LEN(0) == 0)
+	RETFAIL;
+    RETINT((unsigned char)*LA_STR_PTR(0));
 }
