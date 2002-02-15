5bb35f69e08c0dbe6e7a51c2c6c12d51240ff899
Phil Budne <phil@regressive.org>
2002-02-15 15:51:05 +0000

    clean up use new HAVE_ defines!

--- config/win32/config.h
+++ config/win32/config.h
@@ -1,24 +1,32 @@
 /* $Id$ */
 
-/* Windows32 Visual C (and MINGW) config.h */
+/*
+ * Windows32 config.h 1/27/2002
+ *
+ * Used for Borland, MINGW, and MSVC
+ */
 
 #ifndef __STDC__
 /*
- * by default __STDC__ is undefined!?
- *	-Za defines __STDC__ to 1, but that causes includes to turn
- *	off useful non-ANSI extensions
+ * MSVC: by default __STDC__ is undefined!?
+ *	-Za defines __STDC__ to 1, but turns off useful non-ANSI extensions!!
  */
 #define __STDC__	0
 #endif /* __STDC__ not defined */
 
-#define ANSI_STRINGS
-/*#define NEED_POPEN_DECL*/
+/* datatypes; */
 #define SIGFUNC_T	void __cdecl
+
+/* paths; */
 #define SNOLIB_FILE	"snolib.dll"
 #define SNOLIB_DIR	"/snobol"
-#define USE_WINSOCK_H
-#define USE_STDARG_H
 
+/* includes; */
+#define ANSI_STRINGS
+#define HAVE_STDARG_H
+#define HAVE_STDLIB_H
+
+/* non-standard functions; */
 #define finite		_finite
 #define isnan		_isnan
 #define popen		_popen
