ccfde0058899686a44b8dd72f24cf77847787b50
Phil Budne <phil@regressive.org>
2002-02-15 05:51:20 +0000

    ifdef __STDC__ add defines for popen, pclose

--- config/win32/config.h
+++ config/win32/config.h
@@ -1,16 +1,18 @@
 /* $Id$ */
 
-/* Windows32 Visual C config.h */
+/* Windows32 Visual C (and MINGW) config.h */
 
+#ifndef __STDC__
 /*
  * by default __STDC__ is undefined!?
  *	-Za defines __STDC__ to 1, but that causes includes to turn
  *	off useful non-ANSI extensions
  */
 #define __STDC__	0
+#endif /* __STDC__ not defined */
 
 #define ANSI_STRINGS
-#define NEED_POPEN_DECL
+/*#define NEED_POPEN_DECL*/
 #define SIGFUNC_T	void __cdecl
 #define SNOLIB_FILE	"snolib.dll"
 #define SNOLIB_DIR	"/snobol"
@@ -19,3 +21,5 @@
 
 #define finite		_finite
 #define isnan		_isnan
+#define popen		_popen
+#define pclose		_pclose
