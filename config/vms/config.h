a28397a61d7bf4b0d7a3161f66a318c1f7612209
Phil Budne <phil@regressive.org>
2002-02-18 17:34:48 +0000

    comments

--- config/vms/config.h
+++ config/vms/config.h
@@ -1,16 +1,24 @@
 /* $Id$ */
 
 /* VMS config.h 1/26/2002 */
-#define ANSI_STRINGS
 
-#define NEED_POPEN_DECL
+#define ANSI_STRINGS
+/* other include files defines?? HAVE_xxx_H */
+
 #define NO_OFF_T
+
+/* using private popen() simulation (DECC 4.0 lacks popen()) */
+#define NEED_POPEN_DECL
+
+/* use tty_read routines; tty_mode() is a noop */
 #define TTY_READ_COOKED
 #define TTY_READ_RAW
 
 /* 
  * define preprocessor aliases for SIL and snolib subroutine names
- * which conflict with C runtime names (all are upper-case names)
+ * which conflict with C runtime names (all names in the C runtime
+ * are upper-case.  All our source references to C runtime functions
+ * are lower-case, so this just renames our functions to avoid conflict)
  */
 
 #define ANY	XANY
