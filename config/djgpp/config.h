536a4e501f1c2d5bf0dd385739e92d2065cd8138
Phil Budne <phil@regressive.org>
2002-03-01 17:28:03 +0000

    remove unneeded stuff

--- config/djgpp/config.h
+++ config/djgpp/config.h
@@ -7,15 +7,3 @@
 #define TRY_UNDERSCORE
 #define SNOLIB_DIR "/snobol"
 #define SNOLIB_FILE "snolib.dxe"
-
-#define finite(X) 1
-#define isnan(X) 0
-
-/*
- * for generic/sys.c;
- *
- * write a djgpp (dos) version of sys.c
- * using bdos(0x30,0,0) and intdos() w/ function 0x3306
- */
-#define HWNAME "x86"
-#define OSNAME "DOS"
