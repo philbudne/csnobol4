524c0fd4cf4a9dc7a32a9729fbe6ab8cc6bb1668
Phil Budne <phil@regressive.org>
2002-03-15 16:07:29 +0000

    add HAVE_{DXE2,WATTCP} ifdefs add FOLD_HOSTNAMES

--- config/djgpp/config.h
+++ config/djgpp/config.h
@@ -3,6 +3,17 @@
 #define HAVE_STDLIB_H 
 #define HAVE_UNISTD_H 
 
+/* DXE2 support */
+#ifdef HAVE_DXE2
+#define HAVE_OSINIT			/* may not be needed! */
 #define TRY_UNDERSCORE
 #define SNOLIB_DIR "/snobol"
 #define SNOLIB_FILE "snolib.dxe"
+#endif /* HAVE_DXE2 defined */
+
+/* WATTCP support */
+#ifdef HAVE_WATTCP
+#define FOLD_HOSTNAMES
+#define udp_open myudp_open
+#define tcp_open mytcp_open
+#endif /* HAVE_WATTCP defined */
