ebeb443af2e8d802b599e3f5a9db8864511319ba
Phil Budne <phil@regressive.org>
2002-03-20 16:31:49 +0000

    use params

--- pkg/win32/install.bat
+++ pkg/win32/install.bat
@@ -1,17 +1,17 @@
 rem $Id$
 
-mkdir \snobol4
+mkdir %2
 
-copy equ.h \snobol4
-copy include\dt.h \snobol4
-copy include\equ.h \snobol4
-copy include\h.h \snobol4
-copy include\load.h \snobol4
-copy include\macros.h \snobol4
-copy include\snotypes.h \snobol4
-copy config\djgpp\config.h \snobol4
+copy equ.h %2
+copy include\dt.h %2
+copy include\equ.h %2
+copy include\h.h %2
+copy include\load.h %2
+copy include\macros.h %2
+copy include\snotypes.h %2
+copy %1\config.h %2
 
-copy snolib\*.sno \snobol4
-copy host.sno \snobol4
+copy snolib\*.sno %2
+copy host.sno %2
 
-copy snobol4.exe \snobol4
+copy snobol4.exe %2
