6a37a1302e96d45082d328dbb843d9499170ce02
Phil Budne <phil@regressive.org>
2002-03-20 15:25:28 +0000

    copy config.h!!

--- pkg/win32/install.bat
+++ pkg/win32/install.bat
@@ -9,8 +9,9 @@ copy include\h.h \snobol4
 copy include\load.h \snobol4
 copy include\macros.h \snobol4
 copy include\snotypes.h \snobol4
+copy config\djgpp\config.h \snobol4
 
 copy snolib\*.sno \snobol4
 copy host.sno \snobol4
 
-rem copy snobol4.exe \snobol4
+copy snobol4.exe \snobol4
