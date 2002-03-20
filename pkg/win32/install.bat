d6c54e43ce06933527eb31253d28e07b493b94f8
Phil Budne <phil@regressive.org>
2002-03-20 15:21:50 +0000

    Initial revision

---
+++ pkg/win32/install.bat
@@ -0,0 +1,16 @@
+rem $Id$
+
+mkdir \snobol4
+
+copy equ.h \snobol4
+copy include\dt.h \snobol4
+copy include\equ.h \snobol4
+copy include\h.h \snobol4
+copy include\load.h \snobol4
+copy include\macros.h \snobol4
+copy include\snotypes.h \snobol4
+
+copy snolib\*.sno \snobol4
+copy host.sno \snobol4
+
+rem copy snobol4.exe \snobol4
