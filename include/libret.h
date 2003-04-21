0377655602ed2aeb9a03364db53f419337e921cb
Phil Budne <phil@regressive.org>
2003-04-21 19:44:50 +0000

    remove stream[_ret] declarations

--- include/libret.h
+++ include/libret.h
@@ -2,12 +2,6 @@
 
 /* return values for library routines */
 
-enum stream_ret {
-    ST_STOP,
-    ST_EOS,
-    ST_ERROR
-};
-
 enum io_read_ret {
     IO_OK,
     IO_EOF,
@@ -20,6 +14,5 @@ enum io_include_ret {
     INC_OK
 };
 
-enum stream_ret stream __P((struct spec *,struct spec *,struct syntab *));
 enum io_read_ret io_read __P((struct descr *,struct spec *sp));
 enum io_include_ret io_include __P((struct descr *,struct spec *));
