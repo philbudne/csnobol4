4c78e6e1dcd437a2867cf8a01931a2751d897a1f
Phil Budne <phil@regressive.org>
2003-04-22 00:35:25 +0000

    Initial revision

---
+++ include/str.h
@@ -0,0 +1,33 @@
+/* $Id$ */
+
+/* (b)string support */
+
+#ifdef ANSI_STRINGS
+#include <string.h>
+
+#define index strchr
+
+#ifdef USE_MEMCMP
+#define bcmp memcmp
+#endif /* USE_MEMCMP defined */
+
+#ifdef USE_OWN_BCOPY
+/* use lib/aux/bcopy.c (fast, handles overlap correctly) */
+void bcopy __P((const void *, void *, int));
+#endif /* USE_OWN_BCOPY defined */
+
+#ifdef USE_MEMMOVE
+#define bcopy(SRC,DEST,LEN) memmove(DEST,SRC,LEN)
+#endif /* USE_MEMMOVE defined */
+
+#ifdef USE_OWN_BZERO
+/* use lib/aux/bzero.c (faster than memset) */
+void bzero __P((char *, unsigned int));
+#endif /* USE_OWN_BZERO defined */
+
+#else  /* ANSI_STRINGS not defined */
+
+/* there's no place like home! */
+#include <strings.h>
+
+#endif /* ANSI_STRINGS not defined */
