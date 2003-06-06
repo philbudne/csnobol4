3ab09496038f41d170834a3af6e7826a57e056bc
Phil Budne <phil@regressive.org>
2003-06-06 16:11:10 +0000

    flush CONFIG_GUESS

--- lib/generic/sys.c
+++ lib/generic/sys.c
@@ -6,10 +6,6 @@
 #include "config.h"
 #endif /* HAVE_CONFIG_H defined */
 
-#ifndef CONFIG_GUESS
-#define CONFIG_GUESS ""
-#endif /* CONFIG_GUESS not defined */
-
 #include <stdio.h>
 
 #include "h.h"
@@ -21,43 +17,12 @@ void
 hwname(cp)
     char *cp;
 {
-#ifdef HWNAME
     strcpy(cp, HWNAME);
-#else  /* HWNAME not defined */
-    char *sp, *tp;
-    int sl;
-
-    sp = CONFIG_GUESS;
-    sl = 0;
-    tp = sp;
-    while (*tp && *tp++ != '-')
-	sl++;
-
-    if (sl == 0) {			/* no hyphen found */
-	strcpy(cp, "unknown");		/* default value */
-	return;
-    }
-    strncpy(cp, sp, sl);
-    cp[sl] = '\0';
-#endif /* HWNAME not defined */
 }
 
 void
 osname(cp)
     char *cp;
 {
-#ifdef OSNAME
     strcpy(cp, OSNAME);
-#else  /* OSNAME not defined */
-    char *sp;
-
-    /* find last hyphen */
-    sp = rindex(CONFIG_GUESS, '-');
-    if (sp)
-	sp++;
-    if (!sp || !*sp)			/* null or empty string */
-	sp = "unknown";
-
-    strcpy(cp, sp);
-#endif /* OSNAME not defined */
 }
