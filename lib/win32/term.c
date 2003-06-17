a0c55b1316ae5059d95efac5bbdf2a3e166847bc
Phil Budne <phil@regressive.org>
2003-06-17 01:29:33 +0000

    Initial revision

---
+++ lib/win32/term.c
@@ -0,0 +1,21 @@
+/* $Id$ */
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif /* HAVE_CONFIG_H defined */
+
+#include <stdio.h>
+
+#include "h.h"
+#include "snotypes.h"
+#include "lib.h"
+
+/*
+ * return a stdio stream for TERMINAL input variable
+ * Win32 version.
+ */
+
+FILE *
+term_input() {
+    return fopen("CONIN$", "r");
+}
