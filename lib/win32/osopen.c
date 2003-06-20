5d733dab30bef2285e3c6952cab94151fd4c0f24
Phil Budne <phil@regressive.org>
2003-06-20 03:40:12 +0000

    now win32, DJGPP has needed hacks built in

--- lib/win32/osopen.c
+++ lib/win32/osopen.c
@@ -1,7 +1,7 @@
 /* $Id$ */
 
 /*
- * MS-DOS open hook
+ * Win32 open hook
  */
 
 #ifdef HAVE_CONFIG_H
@@ -20,11 +20,11 @@ osdep_open(fname, mode, fpp)
     FILE **fpp;
 {
     if (strcmp(fname, "/dev/tty") == 0) {
-	*fpp = fopen("CON:", mode);
+	*fpp = fopen("CON", mode);
 	return TRUE;			/* matched */
     }
     if (strcmp(fname, "/dev/nul") == 0) {
-	*fpp = fopen("NUL:", mode);
+	*fpp = fopen("NUL", mode);
 	return TRUE;			/* matched */
     }
     return FALSE;			/* no match */
