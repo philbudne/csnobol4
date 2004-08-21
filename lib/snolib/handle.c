d208faceb71dcbd50ed6ee2129609047f57f88ab
Phil Budne <phil@regressive.org>
2004-08-21 05:46:31 +0000

    manage handles for loadables which manage objects

---
+++ lib/snolib/handle.c
@@ -0,0 +1,81 @@
+/* $Id$ */
+
+/*
+ * manage lists of handles
+ */
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif /* HAVE_CONFIG_H defined */
+
+#ifdef HAVE_STDLIB_H                    /* before stdio */
+#include <stdlib.h>                     /* for malloc */
+#else  /* HAVE_STDLIB_H not defined */
+extern void *malloc();
+#endif /* HAVE_STDLIB_H not defined */
+
+#include <stdio.h>			/* for NULL */
+
+#include "snotypes.h"
+#include "h.h"				/* __P() */
+#include "load.h"			/* SNOEXP() */
+#include "handle.h"
+
+void *
+lookup_handle(hlp, h)
+    struct handle_list *hlp;
+    int_t h;
+{
+    struct handle_entry *hp;
+
+    for (hp = hlp->hash[HANDLE_HASH(hp->handle)]; hp; hp = hp->next) {
+	if (hp->handle == h)
+	    return hp->value;
+    }
+    return NULL;
+}
+
+int_t
+new_handle(hlp, vp)
+    struct handle_list *hlp;
+    void *vp;
+{
+    int_t h;
+    struct handle_entry *hp;
+
+    hp = malloc(sizeof(struct handle_entry));
+    if (!hp)
+	return -1;
+
+    do {
+	hp->handle = hlp->next++;
+    } while (hp->handle == BAD_HANDLE || lookup_handle(hlp, hp->handle));
+
+    hp->value = vp;
+    hp->next = hlp->hash[HANDLE_HASH(hp->handle)];
+    hlp->hash[HANDLE_HASH(hp->handle)] = hp;
+    hlp->entries++;
+
+    return hp->handle;
+}
+
+void
+remove_handle(hlp, h)
+    struct handle_list *hlp;
+    int_t h;
+{
+    struct handle_entry *hp, *pp;
+
+    pp = NULL;
+    for (hp = hlp->hash[HANDLE_HASH(hp->handle)]; hp; pp = hp, hp = hp->next) {
+	if (hp->handle == h) {
+	    if (pp)
+		pp->next = hp->next;
+	    else
+		hlp->hash[HANDLE_HASH(hp->handle)] = hp->next;
+	    free(hp);
+	    hlp->entries--;
+	    return;
+	}
+    }
+}
