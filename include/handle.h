68c0cf2586e14098d420fa180dc63a754cdd09c2
Phil Budne <phil@regressive.org>
2004-08-22 14:56:17 +0000

    s/handle_t/snohandle_t/ to avoid conflicts with w32api/rpcdce.h

--- include/handle.h
+++ include/handle.h
@@ -5,20 +5,20 @@
 
 #define BAD_HANDLE -1
 
-typedef int_t handle_t;
+typedef int_t snohandle_t;
 
 struct handle_list {
-    handle_t next;
+    snohandle_t next;
     long entries;
     struct handle_entry *hash[HANDLE_HASH_SIZE];
 };
 
 struct handle_entry {
     struct handle_entry *next;
-    handle_t handle;
+    snohandle_t handle;
     void *value;
 };
 
-SNOEXP(handle_t) new_handle __P((struct handle_list *, void *));
-SNOEXP(void *) lookup_handle __P((struct handle_list *, handle_t));
-SNOEXP(void) remove_handle __P((struct handle_list *, handle_t));
+SNOEXP(snohandle_t) new_handle __P((struct handle_list *, void *));
+SNOEXP(void *) lookup_handle __P((struct handle_list *, snohandle_t));
+SNOEXP(void) remove_handle __P((struct handle_list *, snohandle_t));
