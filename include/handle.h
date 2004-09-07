fa50cced109b99790998816f5f837931669be2c3
Phil Budne <phil@regressive.org>
2004-09-07 16:08:00 +0000

    *** empty log message ***

--- include/handle.h
+++ include/handle.h
@@ -1,22 +1,20 @@
 /* $Id$ */
 
+#define BAD_HANDLE 0
 #define HANDLE_HASH_SIZE (1<<8)		/* power of two */
-#define HANDLE_HASH(H) ((H) & (HANDLE_HASH_SIZE-1))
-
-#define BAD_HANDLE -1
 
 typedef int_t snohandle_t;
 
-struct handle_list {
-    snohandle_t next;
-    long entries;
-    struct handle_entry *hash[HANDLE_HASH_SIZE];
-};
-
+/* internal data structure: */
 struct handle_entry {
     struct handle_entry *next;
     snohandle_t handle;
-    void *value;
+};
+
+/* public data structure */
+struct handle_list {
+    long entries;
+    struct handle_entry *hash[HANDLE_HASH_SIZE];
 };
 
 SNOEXP(snohandle_t) new_handle __P((struct handle_list *, void *));
