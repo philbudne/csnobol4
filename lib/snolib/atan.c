515fd668cdc641be84678f0af40193109e6ccd1c
Phil Budne <phil@regressive.org>
2011-01-24 04:36:25 +0000

    new atan.c

---
+++ lib/snolib/atan.c
@@ -0,0 +1,28 @@
+/* $Id$ */
+
+/*
+ * SPARC SPITBOL compatibility;
+ * LOAD("ATAN(REAL)REAL")
+ * arc tangent
+ */
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif /* HAVE_CONFIG_H defined */
+
+#include <math.h>
+
+#include "h.h"
+#include "snotypes.h"
+#include "macros.h"
+#include "load.h"
+#include "equ.h"
+
+int
+ATAN( LA_ALIST ) LA_DCL
+{
+    real_t ret = atan(LA_REAL(0));
+    if (!finite(ret))
+	RETFAIL;
+    RETREAL(ret);
+}
