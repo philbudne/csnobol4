512619294c5f06c2fddbe47579bc3f47710be108
Phil Budne <phil@regressive.org>
2020-01-25 03:31:58 +0000

    Add run	command! snolib/sdb.sno:	add run	command! sdb.sh: support run command via temporary file! doc/sdb.1.pea: add run command, /tmp file bug

--- sdb.sh
+++ sdb.sh
@@ -7,10 +7,20 @@ SNOBOL4=<BINDIR>/snobol4<VERS>
 # handle user args??
 
 # create listing file, and pass filename in environment so sdb.sno can read it.
-SDB_LISTFILE=/tmp/sdb$$
+SDB_LISTFILE=/tmp/sdb_listing.$$
 export SDB_LISTFILE
 
-# remove listing file on exit (if not already removed by sdb)
-trap "rm -f $SDB_LISTFILE" 0
+SDB_BREAKPOINTS=/tmp/sdb_bkpts.$$
+export SDB_BREAKPOINTS
 
-$SNOBOL4 -b -l $SDB_LISTFILE -L $SDB "$@"
+# remove listing/breakpoints files on exit (if not already removed by sdb)
+trap "rm -f $SDB_LISTFILE $SDB_BREAKPOINTS" 0
+
+while true; do
+    $SNOBOL4 -b -l $SDB_LISTFILE -L $SDB "$@"
+    STATUS=$?
+    if [ ! -f "$SDB_BREAKPOINTS" ]; then
+	exit $STATUS
+    fi
+done
+
