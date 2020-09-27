1265b6433ee56727385d8120529df42256e82bf9
Phil Budne <phil@regressive.org>
2020-09-27 21:54:54 +0000

    use system bzero when using system bcopy

--- include/str.h
+++ include/str.h
@@ -4,15 +4,27 @@
 
 #include <string.h>
 
-#define bzero(A,B) memset(A,0,B)
+/*
+ * 2020-09-27
+ * MOST places still use bcopy/bzero
+ */
 
 #if defined(HAVE_BCOPY)
+
 #include <strings.h>
 #define memmove(DEST,SRC,LEN) bcopy(SRC,DEST,LEN) 
+#define memset(PTR,X,LEN) bzero(PTR, LEN) /* XXX assert X==0?!!! */
+
 #elif defined(NEED_BCOPY)
+
 /* use lib/aux/bcopy.c (fast, handles overlap correctly) */
 void bcopy(const void *, void *, int);
+void bzero(void *, int);
 #define memmove(DEST,SRC,LEN) bcopy(SRC,DEST,LEN) 
+#define memset(PTR,X,LEN) bzero(PTR, LEN) /* XXX assert X==0?!!! */
+
 #elif defined(USE_MEMMOVE)
 #define bcopy(SRC, DEST, LEN) memmove(DEST, SRC, LEN)
+#define bzero(A,B) memset(A,0,B)
+
 #endif /* USE_MEMMOVE defined */
