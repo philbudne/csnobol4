9b6012ba6761e533bd84417eb00325d1d2574122
Phil Budne <phil@regressive.org>
2020-10-10 06:46:13 +0000

    allow zero for handle number

--- include/handle.h
+++ include/handle.h
@@ -4,7 +4,7 @@ typedef struct descr snohandle_t;	/* type of handles returned */
 typedef void *handle_handle_t;		/* handle for handle.c internal use */
 
 #define LA_HANDLE(n) (*LA_DESCR(n))	/* returns snohandle_t */
-#define OK_HANDLE(h) ((h).v != 0 && (h).a.i != 0)
+#define OK_HANDLE(h) ((h).v != 0 && (h).a.i >= 0)
 #define RETHANDLE(h) do { *retval = h; return TRUE; } while(0)
 
 SNOEXP(snohandle_t) new_handle(handle_handle_t *, void *, const char *);
